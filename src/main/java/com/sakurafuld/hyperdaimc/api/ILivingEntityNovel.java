142e80ecf43fdb3fbe1c93d75d1444e9ea69fbb9
Sakurafuld <168341875+Sakurafuld@users.noreply.github.com>
2024-08-02 23:28:47 +0900

    Delete src/main directory

--- src/main/java/com/sakurafuld/hyperdaimc/api/ILivingEntityNovel.java
+++
@@ -1,9 +0,0 @@
-package com.sakurafuld.hyperdaimc.api;
-
-
-import net.minecraft.world.entity.LivingEntity;
-
-public interface ILivingEntityNovel {
-    void novelKill(LivingEntity pEntity);
-    void novelHeartAttack();
-}

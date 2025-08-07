d77ad999d2e3e97ccc484784f11786f8e37d6033
Sakurafuld <sakusaku.special39@gmail.com>
2025-08-07 21:17:30 +0900

    ヴァァァーー－ジョン20000!!!!!!!!!!!その２

---
+++ src/main/java/com/sakurafuld/hyperdaimc/mixin/muteki/KeyboardHandlerMixin.java
@@ -0,0 +1,32 @@
+package com.sakurafuld.hyperdaimc.mixin.muteki;
+
+import com.google.common.base.MoreObjects;
+import com.sakurafuld.hyperdaimc.content.hyper.muteki.MutekiHandler;
+import com.sakurafuld.hyperdaimc.network.HyperConnection;
+import com.sakurafuld.hyperdaimc.network.muteki.ServerboundSpecialGameModeSwitch;
+import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.GameType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(KeyboardHandler.class)
+public abstract class KeyboardHandlerMixin {
+    @Inject(method = "handleDebugKeys", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;chat(Ljava/lang/String;)V", ordinal = 0), cancellable = true)
+    private void handleDebugKeysMuteki$toSpectator(int pKey, CallbackInfoReturnable<Boolean> cir) {
+        if (MutekiHandler.muteki(Minecraft.getInstance().player)) {
+            HyperConnection.INSTANCE.sendToServer(new ServerboundSpecialGameModeSwitch("/gamemode spectator"));
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "handleDebugKeys", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;chat(Ljava/lang/String;)V", ordinal = 1), cancellable = true)
+    private void handleDebugKeysMuteki$fromSpectator(int pKey, CallbackInfoReturnable<Boolean> cir) {
+        if (MutekiHandler.muteki(Minecraft.getInstance().player)) {
+            HyperConnection.INSTANCE.sendToServer(new ServerboundSpecialGameModeSwitch("/gamemode " + MoreObjects.firstNonNull(Minecraft.getInstance().gameMode.getPreviousPlayerMode(), GameType.CREATIVE).getName()));
+            cir.setReturnValue(true);
+        }
+    }
+}

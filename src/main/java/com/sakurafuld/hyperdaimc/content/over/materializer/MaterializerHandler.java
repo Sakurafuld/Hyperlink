698d972b8f1b6b9ef4831c566e8c26bed6a1f8d4
Sakurafuld <sakusaku.special39@gmail.com>
2025-08-09 17:38:09 +0900

    ヴァァァーー－ジョン20000!!!!!!!!!!!その6 もうpreじゃないよ！

--- src/main/java/com/sakurafuld/hyperdaimc/content/over/materializer/MaterializerHandler.java
+++ src/main/java/com/sakurafuld/hyperdaimc/content/over/materializer/MaterializerHandler.java
@@ -106,7 +106,7 @@ public class MaterializerHandler {
             MaterializerRecipeEvent.Add event = new MaterializerRecipeEvent.Add(level, recipe, ingredients);
             MinecraftForge.EVENT_BUS.post(event);
 
-            if (!event.isCanceled()) {
+            if (!event.isCanceled() && !ingredients.isEmpty()) {
                 for (Process process : processes) {
                     if (result.getCount() == process.result().getCount() && ItemHandlerHelper.canItemStacksStack(result, process.result())) {
                         ingredients.forEach(ingredient -> addOrStack(process.ingredients(), ingredient));
@@ -114,9 +114,7 @@ public class MaterializerHandler {
                     }
                 }
 
-                if (!ingredients.isEmpty()) {
-                    processes.add(new Process(result.copy(), ingredients));
-                }
+                processes.add(new Process(result.copy(), ingredients));
             }
         });
 

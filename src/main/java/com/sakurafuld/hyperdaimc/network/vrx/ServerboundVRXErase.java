f7cc1c36ed7f979fbaa30128d3f7381e1cf619f6
Sakurafuld <sakusaku.special39@gmail.com>
2025-08-15 00:04:05 +0900

    2.1その1.1 Ctrl+Alt長押しでGamacrafterのアニメーションをリセットできるよ その他バグバグ修正バグ修正 クライアントハックの対策しつつ、その脆弱性がコンフィグでオンオフ可能になった

--- src/main/java/com/sakurafuld/hyperdaimc/network/vrx/ServerboundVRXErase.java
+++ src/main/java/com/sakurafuld/hyperdaimc/network/vrx/ServerboundVRXErase.java
@@ -1,6 +1,7 @@
 package com.sakurafuld.hyperdaimc.network.vrx;
 
 import com.sakurafuld.hyperdaimc.HyperCommonConfig;
+import com.sakurafuld.hyperdaimc.content.HyperItems;
 import com.sakurafuld.hyperdaimc.content.hyper.vrx.VRXCapability;
 import com.sakurafuld.hyperdaimc.content.hyper.vrx.VRXHandler;
 import com.sakurafuld.hyperdaimc.content.hyper.vrx.VRXSavedData;
@@ -33,36 +34,38 @@ public class ServerboundVRXErase {
     public void handle(Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
             ServerPlayer player = ctx.get().getSender();
-            double reach = Math.max(player.getBlockReach(), player.getEntityReach());
-            Vec3 position = null;
+            if (player.getMainHandItem().is(HyperItems.VRX.get()) && !player.isShiftKeyDown()) {
+                double reach = Math.max(player.getBlockReach(), player.getEntityReach());
+                Vec3 position = null;
 
-            if (this.block) {
-                if (player.pick(reach, 1, false) instanceof BlockHitResult hit && hit.getType() != HitResult.Type.MISS) {
-                    BlockState state = player.level().getBlockState(hit.getBlockPos());
-                    if (state.hasBlockEntity()) {
-                        VRXSavedData data = VRXSavedData.get(player.level());
-                        data.erase(player.getUUID(), hit.getBlockPos(), hit.getDirection());
-                        data.sync2Client(PacketDistributor.DIMENSION.with(player.level()::dimension));
-                        position = Vec3.atCenterOf(hit.getBlockPos());
+                if (this.block) {
+                    if (player.pick(reach, 1, false) instanceof BlockHitResult hit && hit.getType() != HitResult.Type.MISS) {
+                        BlockState state = player.level().getBlockState(hit.getBlockPos());
+                        if (state.hasBlockEntity()) {
+                            VRXSavedData data = VRXSavedData.get(player.level());
+                            data.erase(player.getUUID(), hit.getBlockPos(), hit.getDirection());
+                            data.sync2Client(PacketDistributor.DIMENSION.with(player.level()::dimension));
+                            position = Vec3.atCenterOf(hit.getBlockPos());
+                        }
+                    }
+                } else {
+                    Vec3 start = player.getEyePosition();
+                    Vec3 vector = player.getViewVector(1).scale(reach);
+                    Vec3 end = start.add(vector);
+                    AABB aabb = player.getBoundingBox().expandTowards(vector).inflate(1);
+                    EntityHitResult hit = ProjectileUtil.getEntityHitResult(player, start, end, aabb, entity -> !entity.isSpectator() && entity.isPickable(), reach * reach);
+                    if (hit != null && hit.getType() != HitResult.Type.MISS && (HyperCommonConfig.VRX_PLAYER.get() || !(hit.getEntity() instanceof Player))) {
+                        hit.getEntity().getCapability(VRXCapability.TOKEN).ifPresent(vrx -> {
+                            vrx.erase(player.getUUID());
+                            vrx.sync2Client(hit.getEntity().getId(), PacketDistributor.TRACKING_ENTITY_AND_SELF.with(hit::getEntity));
+                        });
+                        position = hit.getEntity().position();
                     }
                 }
-            } else {
-                Vec3 start = player.getEyePosition();
-                Vec3 vector = player.getViewVector(1).scale(reach);
-                Vec3 end = start.add(vector);
-                AABB aabb = player.getBoundingBox().expandTowards(vector).inflate(1);
-                EntityHitResult hit = ProjectileUtil.getEntityHitResult(player, start, end, aabb, entity -> !entity.isSpectator() && entity.isPickable(), reach * reach);
-                if (hit != null && hit.getType() != HitResult.Type.MISS && (HyperCommonConfig.VRX_PLAYER.get() || !(hit.getEntity() instanceof Player))) {
-                    hit.getEntity().getCapability(VRXCapability.TOKEN).ifPresent(vrx -> {
-                        vrx.erase(player.getUUID());
-                        vrx.sync2Client(hit.getEntity().getId(), PacketDistributor.TRACKING_ENTITY_AND_SELF.with(hit::getEntity));
-                    });
-                    position = hit.getEntity().position();
-                }
-            }
 
-            if (position != null) {
-                VRXHandler.playSound(player.serverLevel(), position, false);
+                if (position != null) {
+                    VRXHandler.playSound(player.serverLevel(), position, false);
+                }
             }
         });
         ctx.get().setPacketHandled(true);
